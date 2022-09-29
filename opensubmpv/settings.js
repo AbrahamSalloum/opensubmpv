c6c129472a91c28e2dadc0bf3fe0033b7ea44fa8
Abraham Salloum <abrahamsalloum@gmail.com>
2022-09-29 16:56:26 +1000

    Update and rename credentials.js to settings.js

---
+++ opensubmpv/settings.js
@@ -0,0 +1,14 @@
+
+//this this is
+var credentials = {
+    "username": "abraham",
+    "password": "password1",
+    "consumerkey": "FAKEYFAKEY1",
+    "token": '', //keep this blank
+    "languages": "en",
+    "keybinding": "ctrl+shift+o",
+}
+
+module.exports =  credentials
+
+module.exports =  credentials
