c7e4a6111e0aa0d80dbe0bb93a7725ee5dd47aac
Abraham Salloum <abrahamsalloum@gmail.com>
2024-02-17 09:13:17 +1100

    Add files via upload

--- opensubmpv/settings.js
+++ opensubmpv/settings.js
@@ -1,9 +1,10 @@
 
 var settings = {
+	minimalInterface: true, // hides unnecessary detail from the interface
     languages: "en", // list of prefered languages, format:  en,cn,ar
     keybinding: "ctrl+shift+o", // keybinding to launch this script
     alwaysDltoTemp: false, //if true always download to temp rather then save to folder
-    autodlMovieHashMatch: false //if true and first result matches moviehash then auto download
+    autodlMovieHashMatch: false, //if true and first result matches moviehash then auto download
 };
 
 module.exports = settings;
